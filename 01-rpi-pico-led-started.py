ddc86c096da856931c7f3d92ba3aa91bdf97a652
simplettrls@gmail.com <simplettrls@gmail.com>
2022-05-22 22:45:43 +0700

    Initial commit

---
+++ 01-rpi-pico-led-started.py
@@ -0,0 +1,13 @@
+from machine import Pin
+import time
+
+led = Pin(25, Pin.OUT)   
+red_led = Pin(15, Pin.OUT) 
+
+while True:
+  led.value(1)            
+    red_led.value(0)
+  time.sleep(1)
+  led.value(0)    
+    red_led.value(1)
+  time.sleep(1)
